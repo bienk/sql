8f3f18a5dbdeca8856bb2b5b6570a96cef5e997e
bienk <bianchi.andrea@iisgalvanimi.edu.it>
2020-02-10 10:45:20 +0000

    06.3 inizio

---
+++ routes/users.js
@@ -0,0 +1,45 @@
+var express = require('express');
+var router = express.Router();
+const sql = require('mssql');
+const config = {
+  user: '4DD_06',  //Vostro user name
+  password: 'xxx123##', //Vostra password
+  server: "213.140.22.237",  //Stringa di connessione
+  database: '4DD_06', //(Nome del DB)
+}
+
+/* GET users listing. */
+router.get('/attori', function(req, res, next) {
+  sql.connect(config, err => {
+    if(err) console.log(err);  // ... error check
+    
+    // Query
+    router.get('/search/:name', function(req, res, next) {
+  sql.connect(config, err => {
+    // ... error check
+    if(err) console.log(err);
+
+    // Query
+    let sqlRequest = new sql.Request();
+    sqlRequest.query(`select * from [School].[Person] where FirstName = '${req.params.name}'`, (err, result) => {
+        // ... error checks
+        if (err) console.log(err);
+
+        res.send(result);
+    });
+  });
+});
+  router.post('/', function(req, res, next){
+    console.log(req.body);
+    res.send(req.body);
+})
+    let sqlRequest = new sql.Request();  //Oggetto che serve a creare le query
+    sqlRequest.query('select * from dbo.AA_ATTORE', (err, result) => {
+        if (err) console.log(err); // ... error checks
+        res.send(result);  //Invio il risultato
+    });
+    
+  });
+});
+
+module.exports = router;
