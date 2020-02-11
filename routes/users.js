b598b8b82604c15e17c0d9a8ec82497c137e605c
bienk <bianchi.andrea@iisgalvanimi.edu.it>
2020-02-11 11:48:15 +0000

    06.3

--- routes/users.js
+++ routes/users.js
@@ -1,45 +1,61 @@
 var express = require('express');
 var router = express.Router();
 const sql = require('mssql');
+
 const config = {
-  user: '4DD_06',  //Vostro user name
-  password: 'xxx123##', //Vostra password
+  user: 'bianchi.andrea',  //Vostro user name
+  password: 'xxx123#', //Vostra password
   server: "213.140.22.237",  //Stringa di connessione
   database: '4DD_06', //(Nome del DB)
 }
 
+//Function to connect to database and execute query
+let executeQuery = function (res, query, next) {
+  sql.connect(config, function (err) {
+    if (err) { //Display error page
+      console.log("Error while connecting database :- " + err);
+      res.status(500).json({success: false, message:'Error while connecting database', error:err});
+      return;
+    }
+    var request = new sql.Request(); // create Request object
+    request.query(query, function (err, result) { //Display error page
+      if (err) {
+        console.log("Error while querying database :- " + err);
+        res.status(500).json({success: false, message:'Error while querying database', error:err});
+        sql.close();
+        return;
+      }
+      res.send(result.recordset);
+      sql.close();
+    });
+
+  });
+}
+
 /* GET users listing. */
-router.get('/attori', function(req, res, next) {
-  sql.connect(config, err => {
-    if(err) console.log(err);  // ... error check
-    
-    // Query
-    router.get('/search/:name', function(req, res, next) {
-  sql.connect(config, err => {
-    // ... error check
-    if(err) console.log(err);
-
-    // Query
-    let sqlRequest = new sql.Request();
-    sqlRequest.query(`select * from [School].[Person] where FirstName = '${req.params.name}'`, (err, result) => {
-        // ... error checks
-        if (err) console.log(err);
-
-        res.send(result);
-    });
-  });
+router.get('/', function (req, res, next) {
+  let sqlQuery = "select * from dbo.[cr-unit-attributes]";
+  executeQuery(res, sqlQuery, next);
 });
-  router.post('/', function(req, res, next){
-    console.log(req.body);
-    res.send(req.body);
-})
-    let sqlRequest = new sql.Request();  //Oggetto che serve a creare le query
-    sqlRequest.query('select * from dbo.AA_ATTORE', (err, result) => {
-        if (err) console.log(err); // ... error checks
-        res.send(result);  //Invio il risultato
-    });
-    
-  });
+
+router.get('/search/:name', function (req, res, next) {
+  let sqlQuery = `select * from dbo.[cr-unit-attributes] where Unit = '${req.params.name}'`;
+  executeQuery(res, sqlQuery, next);
+});
+
+
+router.post('/', function (req, res, next) {
+  // Add a new Unit  
+  let unit = req.body;
+  console.log(unit);
+  if (!unit) {  //Qui dovremmo testare tutti i campi della richiesta
+    res.status(500).json({success: false, message:'Error while connecting database', error:err});
+    return;
+  }
+  let sqlInsert = `INSERT INTO dbo.[cr-unit-attributes] (Unit,Cost,Hit_Speed) 
+                     VALUES ('${unit.Unit}','${unit.Cost}','${unit.Hit_Speed}')`;
+  executeQuery(res, sqlInsert, next);
+  res.send({success:true, message: "unità inserita con successo", unit: unit})
 });
 
 module.exports = router;
